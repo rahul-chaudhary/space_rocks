83d1da661ed735389fcb28b5dd2bccfd3a93e565
Rahul Chaudhary <bossrc64@outlook.com>
2025-03-11 23:18:25 +0530

    UPDATE: PATH for my app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:space_rocks/main.dart';
+import 'package:space_rocks/my_app.dart' show MyApp;
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
